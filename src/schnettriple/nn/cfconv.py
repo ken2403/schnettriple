b26e6588807f9de5bdb4c00aef68b84ee990a177
ken2403 <ken2403.pmtdg@gmail.com>
2021-11-04 00:03:17 +0900

    modify for triple mapping

--- src/schnettriple/nn/cfconv.py
+++ src/schnettriple/nn/cfconv.py
@@ -3,13 +3,15 @@ from torch import nn
 from schnetpack.nn import Dense
 from schnetpack.nn.base import Aggregate
 
+from schnettriple.nn.mapping import TripleMapping
+
 
 __all__ = ['CFConvTriple']
 
 
 class CFConvTriple(nn.Module):
     """
-    Continuous-filter convolution block used in SchNet module.
+    Continuous-filter convolution block used in SchNetTriple module.
 
     Attributes
     ----------
@@ -19,12 +21,20 @@ class CFConvTriple(nn.Module):
             number of filter dimensions.
         n_out : int
             number of output dimensions.
-        filter_network : nn.Module
-            filter block.
+        max_zeta : int
+
+        n_zeta : int
+
+        filter_network_double : nn.Module
+            filter block for double propperties.
+        filter_network_triple : nn.Module
+            filter block for triple properties.
         cutoff_network : nn.Module, optional, default=None
             if None, no cut off function is used.
         activation : callable, optional, default=None
             if None, no activation function is used.
+        crossterm : bool, default=False
+            if True,
         normalize_filter : bool, optional, default=False
             If True, normalize filter to the number of
             neighbors when aggregating.
@@ -37,22 +47,32 @@ class CFConvTriple(nn.Module):
         n_in,
         n_filters,
         n_out,
-        filter_network,
+        max_zeta,
+        n_zeta,
+        filter_network_double,
+        filter_network_triple,
         cutoff_network=None,
         activation=None,
+        crossterm=False,
         normalize_filter=False,
         axis=2,
     ):
         super(CFConvTriple, self).__init__()
         self.in2f = Dense(n_in, n_filters, bias=False, activation=None)
         self.f2out = Dense(n_filters, n_out, bias=True, activation=activation)
-        self.filter_network = filter_network
+        self.triple_mapping = TripleMapping(
+            max_zeta=max_zeta, n_zeta=n_zeta, crossterm=crossterm
+        )
+        self.filter_network_double = filter_network_double
+        self.filter_network_triple = filter_network_triple
         self.cutoff_network = cutoff_network
+        self.crossterm = crossterm
         self.agg = Aggregate(axis=axis, mean=normalize_filter)
 
     def forward(
-        self, x, r_ij, r_ik, r_jk, neighbors_j, neighbors_k
-        triple_mask, f_ij=None, f_ik=None, f_jk=None):
+        self, x, r_ij, r_ik, r_jk, neighbors_j, triple_masks,
+        f_ij=None, f_ik=None, f_jk=None
+    ):
         """
         Compute convolution block.
 
@@ -61,68 +81,62 @@ class CFConvTriple(nn.Module):
             x : torch.Tensor
                 input representation/embedding of atomic environments
                 with (N_b, N_a, n_in) shape.
-            r_ij : torch.Tensor
+            r_ij: torch.Tensor
                 interatomic distances from the centered atom i
                 to the neighbor atom j of (N_b, N_a, N_nbh) shape.
             r_ik : torch.Tensor
                 interatomic distances from the centered atom i
                 to the neighbor atom k of (N_b, N_a, N_nbh) shape.
-            r_jk : torch.Tensor
+            r_jk: torch.Tensor, optional, default=None
                 interatomic distances from the neighbor atom j
-                to  the neighbor atom k of (N_b, N_a, N_nbh) shape.
+                to the neighbor atom k of (N_b, N_a, N_nbh) shape.
             neighbors_j : torch.Tensor
                 of (N_b, N_a, N_nbh) shape.
-            neighbors_k : torch.Tensor
-
-            triple_mask : torch.Tensor
+            triple_masks : torch.Tensor
                 mask to filter out non-existing neighbors
                 introduced via padding.
-            f_ij : torch.Tensor, optional, default=None
+            f_ij: torch.Tensor, optional, default=None
                 expanded interatomic distances in a basis.
-                If None, r_ij.unsqueeze(-1) is used.
-            f_ik : torch.Tensor, optional, default=None
+                If None, r_ij_double.unsqueeze(-1) is used.
+            f_ik: torch.Tensor, optional, default=None
                 expanded interatomic distances in a basis.
-                If None, r_ik.unsqueeze(-1) is used.
-            f_jk : torch.Tensor, optional, default=None
+                If None, r_ij_double.unsqueeze(-1) is used.
+            f_jk: torch.Tensor, optional, default=None
                 expanded interatomic distances in a basis.
-                If None, r_jk.unsqueeze(-1) is used.
+                If None, r_ij_double.unsqueeze(-1) is used.
 
         Returns
         -------
             torch.Tensor
-                block output with (N_b, N_a, n_out) shape.
+                block output with (N_batch, N_atoms, N_out) shape.
 
         """
         if f_ij is None:
             f_ij = r_ij.unsqueeze(-1)
-
         if f_ik is None:
             f_ik = r_ik.unsqueeze(-1)
+        if self.crossterm:
+            if f_jk is None:
+                f_jk = r_jk.unsqueeze(-1)
 
-        if f_jk is None:
-            f_jk = r_jk.unsqueeze(-1)
-
-        # pass expanded interactomic distances through filter block
-        W_ij = self.filter_network(f_ij)
+        # pass expanded interactomic distances through filter block (double)
+        W_ij_double = self.filter_network_double(f_ij)
         # apply cutoff
         if self.cutoff_network is not None:
             C_ij = self.cutoff_network(r_ij)
-            W_ij = W_ij * C_ij.unsqueeze(-1)
+            W_ij_double = W_ij_double * C_ij.unsqueeze(-1)
 
-        # pass expanded interactomic distances through filter block
-        W_ik = self.filter_network(f_ik)
+        # calculate triple mapping
+        m_ijk = self.triple_mapping(r_ij, r_ik, r_jk, f_ij, f_ik, f_jk, triple_masks)
+        # pass triple mapping through filter block (triple)
+        W_ijk = self.filter_network_triple(m_ijk)
         # apply cutoff
         if self.cutoff_network is not None:
             C_ik = self.cutoff_network(r_ik)
-            W_ik = W_ik * C_ik.unsqueeze(-1)
-
-        # pass expanded interactomic distances through filter block
-        W_jk = self.filter_network(f_jk)
-        # apply cutoff
-        if self.cutoff_network is not None:
-            # Whether cutoff should be applied or not
-            C_jk = self.cutoff_network(r_jk)
-            W_jk = W_jk * C_jk.unsqueeze(-1)
+            W_ijk = W_ijk * C_ij.unsqueeze(-1) * C_ik.unsqueeze(-1)
+            if self.crossterm:
+                C_jk = self.cutoff_network(r_jk)
+                W_ijk *= C_jk.unsqueeze(-1)
 
         # pass initial embeddings through Dense layer
         y = self.in2f(x)
@@ -135,8 +149,8 @@ class CFConvTriple(nn.Module):
         y = y.view(nbh_size[0], nbh_size[1], nbh_size[2], -1)
 
         # element-wise multiplication, aggregating and Dense layer
-        y = y * W_ij * W_ik * W_jk    # !!!my modify!!!
-        y = self.agg(y, triple_mask)
+        y = y * W_ij_double * W_ijk
+        y = self.agg(y, triple_masks)
         y = self.f2out(y)
 
         return y
