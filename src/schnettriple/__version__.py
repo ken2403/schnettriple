d34cb62fd7ddcd82e2b2bedf44c83e0bdbeaa31c
ken2403 <ken2403.pmtdg@gmail.com>
2021-11-07 09:23:57 +0900

    v0.1.4

--- src/schnettriple/__version__.py
+++ src/schnettriple/__version__.py
@@ -4,5 +4,6 @@ __version_info__ = (0, 1, 4)
 - reshapeとaggを2体と3体とで別々に行う。
 - 学習の際のparameterを小さくする(featuresとnum_gaussinas)ことでモデルサイズを抑える
 - filter networkの2個目の層にもactivationを追加
+- zetaをregister_bufferに入れる
 """
 __version__ = ".".join(map(str, __version_info__))
