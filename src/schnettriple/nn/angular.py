370927bb5fbe0108f1256f3a079f1f2db1387d5d
ken2403 <ken2403.pmtdg@gmail.com>
2021-12-07 22:25:39 +0900

    bagfix; apply triple_mask for cos_theta

--- src/schnettriple/nn/angular.py
+++ src/schnettriple/nn/angular.py
@@ -123,8 +123,8 @@ class AngularDistribution(nn.Module):
         cos_theta = (torch.pow(r_ij, 2) + torch.pow(r_ik, 2) - torch.pow(r_jk, 2)) / (
             2.0 * r_ij * r_ik
         )
-        # if triple_mask is not None:
-        #     cos_theta[triple_mask == 0] = 0.0
+        if triple_mask is not None:
+            cos_theta[triple_mask == 0] = 0.0
         angular_filter = self.theta_filter(cos_theta)
 
         if triple_mask is not None:
