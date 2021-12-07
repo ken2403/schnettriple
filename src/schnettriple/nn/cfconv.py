bab3ccad496be0743649ecf9c6e0e6df0cd6ad64
ken2403 <ken2403.pmtdg@gmail.com>
2021-12-07 19:28:48 +0900

    modify r_ij and r_ik

--- src/schnettriple/nn/cfconv.py
+++ src/schnettriple/nn/cfconv.py
@@ -145,11 +145,11 @@ class CFConvTriple(nn.Module):
         _, _, Nbr_tirple = neighbors_j.size()
         nbh_j = neighbors_j.reshape(-1, At * Nbr_tirple, 1)
         nbh_j = nbh_j.expand(-1, -1, y.size(2))
-        r_ij = r_ij.reshape(-1, At * Nbr_tirple, 1).expand(-1, -1, y.size(2))
+        r_ij = r_ij.view(-1, At, Nbr_tirple, 1)
 
         nbh_k = neighbors_k.reshape(-1, At * Nbr_tirple, 1)
         nbh_k = nbh_k.expand(-1, -1, y.size(2))
-        r_ik = r_ik.reshape(-1, At * Nbr_tirple, 1).expand(-1, -1, y.size(2))
+        r_ik = r_ik.view(-1, At, Nbr_tirple, 1)
         # get j and k neighbors of i. Add these atomic embeddings.
         y_triple = (
             r_ij * torch.gather(y, 1, nbh_j) + r_ik * torch.gather(y, 1, nbh_k)
