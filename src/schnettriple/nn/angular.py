11bf6abe3f524dee9f273fb86ddd509ca5fb0d4d
ken2403 <ken2403.pmtdg@gmail.com>
2021-12-10 01:07:50 +0900

    modify sqrt

--- src/schnettriple/nn/angular.py
+++ src/schnettriple/nn/angular.py
@@ -55,7 +55,7 @@ class ThetaDistribution(nn.Module):
         # theta_distribution = 2 ** (1.0 - self.zeta) * torch.pow(
         #     1.0 + torch.cos(diff_theta), self.zeta
         # )
-        sin_theta = torch.sqrt(1.0 - cos_theta ** 2)
+        sin_theta = torch.sqrt(1.0 - cos_theta ** 2 + 1e-5)
         sin_theta[cos_theta == -1] = 0.0
         diff_cos = cos_theta[:, :, :, None] * torch.cos(
             self.offset_theta[None, None, None, :]
