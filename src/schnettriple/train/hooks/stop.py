1dc3e6aa3a49928b96099d25ec2027599fe2d7e8
ken2403 <ken2403.pmtdg@gmail.com>
2021-12-09 21:37:12 +0900

    add NaNstoperror

--- src/schnettriple/train/hooks/stop.py
+++ src/schnettriple/train/hooks/stop.py
@@ -1,15 +1,15 @@
 from schnetpack.train.hooks import Hook
 
 
-__all__ = ["NanStoppingHook"]
+__all__ = ["NaNStoppingHook"]
 
 
-class NanStopError(Exception):
+class NaNStopError(Exception):
     pass
 
 
-class NanStoppingHook(Hook):
+class NaNStoppingHook(Hook):
     def on_batch_end(self, trainer, train_batch, result, loss):
-        if loss.isnan():
+        if loss.isnan().any():
             trainer._stop = True
-            raise NanStopError("The value of loss has become nan! Stop training.")
+            raise NaNStopError("The value of loss has become nan! Stop training.")
