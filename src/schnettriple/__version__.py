33f4bf8f2a3a05188f4a208173d4f53b68199182
ken2403 <ken2403.pmtdg@gmail.com>
2021-12-25 11:51:45 +0900

    v2.1.3

--- src/schnettriple/__version__.py
+++ src/schnettriple/__version__.py
@@ -1,4 +1,4 @@
-__version_info__ = (2, 1, 2)
+__version_info__ = (2, 1, 3)
 __version__ = ".".join(map(str, __version_info__))
 """
 (0.1.5)
@@ -69,4 +69,6 @@ __version__ = ".".join(map(str, __version_info__))
 (2.1.2)
 - cutoff apply before filter generator
 - change angular term to ANI-1
+(2.1.3)
+- add trainble_gaussian parameter
 """
